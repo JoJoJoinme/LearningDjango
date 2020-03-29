40d275acf12a0c213d08848a5ddfcdb7b2ee03d9
Helious.wu <helious.wu@foxmail.com>
2020-03-29 09:48:29 +0800

    The first wrong try

---
+++ blog/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.utils import timezone
+from .models import Post, Category, Tag
+
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+    fields = ['title', 'body', 'excert', 'category', 'tags']
+
+
+def save_model(self, request, obj, form, change):
+    obj.author = request.user
+    super().save_model(request, obj, form, change)
+
+
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Tag)
---
+++ blog/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class BlogConfig(AppConfig):
+    name = 'blog'
+    verbose_name = '博客'
+
+# class GroupConfig(AppConfig):
+#     name = "Authentication and Authorization"
+    # name = 'authentication and authorization'
+    # verbose_name = '用户管理'

d865517098f0b3bc14287133cd6c6bc32017f929
Helious.wu <helious.wu@foxmail.com>
2020-04-15 18:30:11 +0800

    modified elasticsearch

---
+++ blog/utils.py
@@ -0,0 +1,15 @@
+from django.utils.html import strip_tags
+from haystack.utils import Highlighter as HaystackHightlighter
+
+class Highlighter(HaystackHightlighter):
+    '''
+    自定义关键词高亮器，不截断果断的文本
+    '''
+
+    def highlight(self, text_block):
+        self.text_block = strip_tags(text_block)
+        highlight_locations = self.find_highlightable_words()
+        start_offset, end_offset = self.find_window(highlight_locations)
+        if len(text_block) < self.max_length:
+            start_offset = 0
+        return self.render_html(highlight_locations, start_offset, end_offset)

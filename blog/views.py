d55b1ddb1568cb844497143174b4ac72e8b5875d
Helious.wu <helious.wu@foxmail.com>
2020-04-15 18:21:10 +0800

    add the new feature of search

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 import re
 
+from django.contrib import messages
+from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 from django.shortcuts import render
 from django.utils.text import slugify
 import markdown
+from django.views.generic import ListView, DetailView
 from markdown.extensions.toc import TocExtension
+from pure_pagination import PaginationMixin
 
 from .models import Post, Category, Tag
 import markdown
@@ -18,12 +22,7 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    # post.body = markdown.markdown(post.body,
-    #                               extensions=[
-    #                                   'markdown.extensions.extra',
-    #                                   'markdown.extensions.codehilite',
-    #                                   'markdown.extensions.toc',
-    #                               ])
+    post.increase_views()  # 阅读量+1
     md = markdown.Markdown(extensions=[
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
@@ -52,3 +51,70 @@ def tag(request, pk):
     t = get_object_or_404(Tag, pk=pk)
     post_list =Post.objects.filter(tags=t).order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+class IndexView(PaginationMixin, ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    paginate_by = 10
+
+class CategoryView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+class ArchiveView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        return super(ArchiveView, self).get_queryset().filter(created_time__year=self.kwargs.get('year'),
+                                                              created_time__month=self.kwargs.get('month')).order_by('-created_time')
+
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        t = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=t).order_by('created_time')
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+    # def get_object(self, queryset=None):
+    #     post = super().get_object(queryset=None)
+    #     md = markdown.Markdown(extensions=[
+    #         'markdown.extensions.extra',
+    #         'markdown.extensions.codehilite',
+    #         TocExtension(slugify=slugify)
+    #     ])
+    #     post.body = md.convert(post.body)
+    #     m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    #     post.toc = m.group(1) if m is not None else ''
+    #     return post
+
+def search(request):
+    q = request.GET.get('q')
+
+    if not q:
+        error_msg = "请输入关键词"
+        messages.add_message(request, messages.ERROR, error_msg, extra_tags='danger')
+        return redirect('blog:index')
+
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request, 'blog/index.html', {'post_list': post_list})

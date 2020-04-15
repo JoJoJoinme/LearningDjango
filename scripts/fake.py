975c37a6749f605a094d0d65c8feec45bef5d72c
Helious.wu <helious.wu@foxmail.com>
2020-04-15 18:31:16 +0800

    add fake test to generate post

---
+++ scripts/fake.py
@@ -0,0 +1,102 @@
+import os
+import pathlib
+import random
+import sys
+from datetime import timedelta
+
+import django
+import faker
+from django.utils import timezone
+
+# 将项目根目录添加到Python的模块搜索路径
+back = os.path.dirname
+BASE_DIR = back(back(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+if __name__ == '__main__':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PersonalBlog.settings.local")
+    django.setup()
+
+    from blog.models import Post, Category, Tag
+    from comments.models import Comment
+    from django.contrib.auth.models import User
+
+    print('clean database')
+    Post.objects.all().delete()
+    Category.objects.all().delete()
+    Tag.objects.all().delete()
+    Comment.objects.all().delete()
+    User.objects.all().delete()
+
+    print('create a blog user')
+    user = User.objects.create_superuser('admin', 'admin@hellogithub.com', 'admin')
+    category_list = ['Python学习笔记', '开源项目', '工具资源', '生活杂谈', 'test category']
+    tag_list = ['Python', 'Django', 'pipenv', 'Docker', 'Nginx', 'ElasticSearch']
+    a_year_ago = timezone.now() - timedelta(days=365)
+
+    print('create Categorys and Tags')
+    for cate in category_list:
+        Category.objects.create(name=cate)
+
+    for tag in tag_list:
+        Tag.objects.create(name=tag)
+
+    # print('create a markdown sample post')
+    # Post.objects.create(
+    #     title='Markdown AND Highlite Test',
+    #     body=pathlib.Path(BASE_DIR).joinpath('scripts', 'md.sample').read_text(encoding='utf-8'),
+    #     category=Category.objects.create(name='Markdown Writing Test'),
+    #     author=user,
+    # )
+    # print('create some faked posts published within the past year')
+    fake = faker.Faker() # create English
+    for _ in range(100):
+        tags = Tag.objects.order_by('?')
+        tag1 = tags.first()
+        tag2 = tags.last()
+        cate = Category.objects.order_by('?').first()
+        created_time = fake.date_time_between(start_date='-1y', end_date='now', tzinfo=timezone.get_current_timezone())
+        post = Post.objects.create(
+            title=fake.sentence().rstrip('.'),
+            body='\n\n'.join(fake.paragraphs(10)),
+            created_time=created_time,
+            category=cate,
+            author=user,
+        )
+        post.tags.add(tag1, tag2)
+        post.save()
+    print('Generate Chinese fake article')
+    fake = faker.Faker('zh_CN')
+    for _ in range(100):
+        tags = Tag.objects.order_by('?')
+        tag1 = tags.first()
+        tag2 = tags.last()
+        cate = Category.objects.order_by('?').first()
+        created_time = fake.date_time_between(start_date='-1y', end_date='now', tzinfo=timezone.get_current_timezone())
+        post = Post.objects.create(
+            title=fake.sentence().rstrip('.'),
+            body='\n\n'.join(fake.paragraphs(10)),
+            created_time=created_time,
+            category=cate,
+            author=user,
+        )
+        post.tags.add(tag1, tag2)
+        post.save()
+    print('Generating Chinese article finished')
+    print('create some comments')
+    for post in Post.objects.all()[:20]:
+        post_created_time = post.created_time
+        delta_in_days = '-' + str((timezone.now() - post_created_time).days) + 'd'
+        for _ in range(random.randrange(3, 15)):
+            Comment.objects.create(
+                name=fake.name(),
+                email=fake.email(),
+                url=fake.uri(),
+                text=fake.paragraph(),
+                created_time=fake.date_time_between(
+                    start_date=delta_in_days,
+                    end_date="now",
+                    tzinfo=timezone.get_current_timezone()),
+                post=post,
+            )
+    print('done!')

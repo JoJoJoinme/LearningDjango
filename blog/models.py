a0aef79b0f0c5e51790bf39d4acfd8ddcb383872
Helious.wu <helious.wu@foxmail.com>
2020-04-15 18:23:36 +0800

    add new feature

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,18 @@
+import re
+
+import markdown
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
+from django.utils.functional import cached_property
+# from markdown.extensions import toc
+from markdown.extensions import toc
+from markdown.extensions.toc import TocExtension, slugify
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -24,14 +33,15 @@ class Tag(models.Model):
         return self.name
 
 class Post(models.Model):
-    title = models.CharField('标题', max_length=70)  #标题
+    title = models.CharField('标题', max_length=70)  # 标题
     body = models.TextField('正文')  # 内容
     created_time = models.DateTimeField('创建时间')  # 创建时间
     modified_time = models.DateTimeField('修改时间')   # 修改时间
-    excerpt = models.CharField('摘要', max_length=200, blank=True)   #文章摘要
+    excerpt = models.CharField('摘要', max_length=200, blank=True)   # 文章摘要
     category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
     author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+    views = models.PositiveIntegerField(default=0, editable=False)
 
     def save(self, *args, **kwargs):
         self.modified_time = timezone.now()
@@ -48,3 +58,30 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
 
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+    @property
+    def toc(self):
+        return self.rich_content.get("toc", "")
+
+    @property
+    def body_html(self):
+        return self.rich_content.get("content", "")
+
+    @cached_property
+    def rich_content(self):
+        return generate_rich_content(self.body)
+
+def generate_rich_content(value):
+    md = markdown.Markdown(
+        extensions=[
+            "markdown.extensions.extra",
+            "markdown.extensions.codehilite",
+            TocExtension(slugify=slugify)
+        ]
+    )
+    content = md.convert(value)
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    return {"content": content, "toc": toc}

6c75d6fded04a312aaadf3704e777f88eefa2ca3
Helious.wu <helious.wu@foxmail.com>
2020-04-15 18:19:56 +0800

    add search

--- PersonalBlog/urls.py
+++ PersonalBlog/urls.py
@@ -1,4 +1,4 @@
-"""PerosnalBlog URL Configuration
+"""PersonalBlog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from blog.feeds import AllPostsRssFeed
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('', include('comments.urls'))
+    path('', include('comments.urls')),
+    path('all/rss/', AllPostsRssFeed(), name='rss'),
+    path('search/', include('haystack.urls')),
 ]
